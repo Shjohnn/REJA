28df62ff42f316ea0cd9723778ca398ca3e94212
Shukurjon Nematov <shjonbek1702@gmail.com>
2026-08-29 17:57:28 +0900

    22 Asynchronus functionlarni qollash

--- train.js
+++ train.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 console.log("TRAINING AREA");
 
 console.log("Jack Ma maslahatlari");
@@ -10,23 +12,47 @@ const list = [
     'endi dam oling, foydasi yoq' //60+
 ];
 
-function maslahatBering(a, callback) {
-    if (typeof a !== 'number') callback('insert a number',null);
-    else if (a<=20 ) callback(null, list[0]);
-    else if (a<=30 ) callback(null, list[1]);
-    else if (a<=40 ) callback(null, list[2]);
-    else if (a<=50 ) callback(null, list[3]);
-    else if (a<=60 ) callback(null, list[4]);
-    else setTimeout(function() {
-        callback(null, list[5]);
-    }, 5000);
+ async function maslahatBering(a) {
+    if (typeof a !== 'number') throw new Error('insert a number',null);
+    else if (a<=20 ) return list[0];
+    else if (a<=30 ) return(list[1]);
+    else if (a<=40 ) return(list[2]);
+    else if (a<=50 ) return(list[3]);
+    else if (a<=60 ) return(list[4]);
+    else {
+        return new Promise((resolve, reject) => {
+            setInterval(() => {
+                resolve(list[5]);
+            }   , 1000);       
+        }); 
+    }
 }
 
-console.log("passed here 0 ");
+    // else setTimeout(function() {
+    //     callback(null, list[5]);
+    // }, 5000);
 
-maslahatBering(25, function(err, data) {
-    if (err) console.log(err);
-    else console.log('javob:', data);
-});
 
-console.log("passed here 1");
+
+
+//then, catch bilan ishlash##################
+//  console.log('passed here 0');
+
+//  maslahatBering(55)
+// .then((result) => {
+//     console.log('javob: ', result);
+// })
+// .catch((error) => {
+//     console.error('xatolik: ', error);
+// });
+// console.log('passed here 1');   
+
+//async, await bilan ishlash##################
+async function run() {
+    let javob = await maslahatBering(20);
+    console.log('javob: ', javob);
+    javob = await maslahatBering(31);
+    console.log('javob: ', javob);
+ 
+}
+run();
