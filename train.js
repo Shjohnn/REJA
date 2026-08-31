75bf535d748802f409d1c372c87986ce123edc9a
Shukurjon Nematov <shjonbek1702@gmail.com>
2026-09-01 00:09:03 +0900

    TASK B

--- train.js
+++ train.js
@@ -1,4 +1,24 @@
 console.log("TRAINING AREA");
+//TASK B: 
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+let amount = 0;
+function countNumber( input) {
+    for (let i=0; i<input.length; i++) {
+        if (input[i] >= '0' && input[i] <= '9') {
+            amount++;
+        }
+    }
+};
+
+countNumber('222ddd')
+console.log(amount)
+
+
+
+
 
 // TASK A
 
