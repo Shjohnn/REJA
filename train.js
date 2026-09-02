7c081caa02b0d1910ded5cd239e33846257824cc
Shukurjon Nematov <shjonbek1702@gmail.com>
2026-09-03 00:30:19 +0900

    TASK C

--- train.js
+++ train.js
@@ -1,20 +1,76 @@
 console.log("TRAINING AREA");
+// MITASK-C 
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, 
+// hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non,
+//  5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 
+// 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+const moment = require("moment");
+
+class Shop {
+    constructor(non, lagmon, cola) {
+        this.non = non;
+        this.lagmon = lagmon;
+        this.cola = cola;
+    }
+
+    qoldiq() {
+        console.log(`Hozir ${moment().format("HH:mm")}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
+    }
+
+    sotish(mahsulot, miqdor) {
+        this[mahsulot] -= miqdor;
+
+        console.log(`${moment().format("HH:mm")} — ${miqdor}ta ${mahsulot} sotildi`);
+    }
+
+    qabul(mahsulot, miqdor) {
+        this[mahsulot] += miqdor;
+
+        console.log(`${moment().format("HH:mm")} — ${miqdor}ta ${mahsulot} qabul qilindi`);
+    }
+}
+
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq();
+
+shop.sotish("non", 3);
+shop.qabul("cola", 4);
+
+shop.qoldiq();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //TASK B: 
 
-// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
-// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+// // Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// // MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 
-let amount = 0;
-function countNumber( input) {
-    for (let i=0; i<input.length; i++) {
-        if (input[i] >= '0' && input[i] <= '9') {
-            amount++;
-        }
-    }
-};
+// let amount = 0;
+// function countNumber( input) {
+//     for (let i=0; i<input.length; i++) {
+//         if (input[i] >= '0' && input[i] <= '9') {
+//             amount++;
+//         }
+//     }
+// };
 
-countNumber('222ddd')
-console.log(amount)
+// countNumber('222ddd')
+// console.log(amount)
 
 
 
