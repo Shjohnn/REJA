3bc6f7c37b183175698105d7b2f5f05e0560b872
Shukurjon Nematov <shjonbek1702@gmail.com>
2026-09-08 01:13:46 +0900

    TASK D

--- train.js
+++ train.js
@@ -1,4 +1,56 @@
 console.log("TRAINING AREA");
+// TASK D : 
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin,
+//  hamda agar har ikkala string bir hil harflardan iborat
+//   bolsa true aks holda false qaytarsin
+
+// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+
+
+
+function checkContent(str1, str2) {
+    let firstString = str1.split("").sort().join("");
+    let secondString = str2.split("").sort().join("");
+
+    if (firstString === secondString) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(checkContent("mitgroup", "gmtiprou")); // true
+
+console.log(checkContent("tyler", "john")); // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // MITASK-C 
 
 // Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, 
@@ -6,40 +58,40 @@ console.log("TRAINING AREA");
 // MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non,
 //  5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 
 // 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
-const moment = require("moment");
+// const moment = require("moment");
 
-class Shop {
-    constructor(non, lagmon, cola) {
-        this.non = non;
-        this.lagmon = lagmon;
-        this.cola = cola;
-    }
+// class Shop {
+//     constructor(non, lagmon, cola) {
+//         this.non = non;
+//         this.lagmon = lagmon;
+//         this.cola = cola;
+//     }
 
-    qoldiq() {
-        console.log(`Hozir ${moment().format("HH:mm")}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
-    }
+//     qoldiq() {
+//         console.log(`Hozir ${moment().format("HH:mm")}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
+//     }
 
-    sotish(mahsulot, miqdor) {
-        this[mahsulot] -= miqdor;
+//     sotish(mahsulot, miqdor) {
+//         this[mahsulot] -= miqdor;
 
-        console.log(`${moment().format("HH:mm")} — ${miqdor}ta ${mahsulot} sotildi`);
-    }
+//         console.log(`${moment().format("HH:mm")} — ${miqdor}ta ${mahsulot} sotildi`);
+//     }
 
-    qabul(mahsulot, miqdor) {
-        this[mahsulot] += miqdor;
+//     qabul(mahsulot, miqdor) {
+//         this[mahsulot] += miqdor;
 
-        console.log(`${moment().format("HH:mm")} — ${miqdor}ta ${mahsulot} qabul qilindi`);
-    }
-}
+//         console.log(`${moment().format("HH:mm")} — ${miqdor}ta ${mahsulot} qabul qilindi`);
+//     }
+// }
 
-const shop = new Shop(4, 5, 2);
+// const shop = new Shop(4, 5, 2);
 
-shop.qoldiq();
+// shop.qoldiq();
 
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
+// shop.sotish("non", 3);
+// shop.qabul("cola", 4);
 
-shop.qoldiq();
+// shop.qoldiq();
 
 
 
